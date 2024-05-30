3be2a99ae8bae6d854034cd9237a6e87f6c362d5
daniprimo <daniellopes0208@hotmail.com>
2024-05-30 13:55:01 -0300

    Commit de configurações

---
+++ src/main/java/com/gerencial/projeto/aplicacao/MsPmiGerencialProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.gerencial.projeto.aplicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPmiGerencialProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPmiGerencialProjetoApplication.class, args);
+	}
+
+}
